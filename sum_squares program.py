d59e78a1cd93b00207d825b9695760181a9d854a
haiqa31 <151655846+haiqa31@users.noreply.github.com>
2023-12-22 19:55:27 +0500

    Add files via upload

---
+++ sum_squares program.py
@@ -0,0 +1,7 @@
+#sum_squares program
+n1=int(input("enter a number"))
+n2=int(input("enter a number"))
+if n1%2!=0 and n2%2!=0: 
+     sum_squares=(n1+n1)+(n2+n2) 
+     print("sum_squares",sum_squares)
+else: ("The calculation is not performed")
